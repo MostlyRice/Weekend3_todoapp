712498ac9d01ed2141a0f8d9e6843af8caca4a91
Zygy93 <amazinzygy@gmail.com>
2018-02-18 16:13:35 -0600

    updated pools

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,14 +1,13 @@
 const pg = require('pg');
 const Pool = pg.Pool;
 // This configures our pool.  Only need to change database name:
- const config = {
-   database: 'taskmaster', // change this for each app/   host: 'localhost',
-   port: 5432,
-   max: 10,
-   idleTimeoutMillis: 5000 // 5 seconds
+const config = {
+  database: 'taskmaster',
+  host: 'localhost',
+  port: 5432,
+  max: 10,
+  idleTimeoutMillis: 5000 // 5 seconds
 }
-
-// create a pool
 const pool = new Pool(config);
 
 module.exports = pool;
